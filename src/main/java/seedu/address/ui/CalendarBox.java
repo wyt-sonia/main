64db1ef4f3c31dc96842a423b6de03e8f63cefa2
gx-huang <ahuanggx@gmail.com>
2020-03-21 18:06:49 +0800

    Checkstyle

--- src/main/java/seedu/address/ui/CalendarBox.java
+++ src/main/java/seedu/address/ui/CalendarBox.java
@@ -1,5 +1,11 @@
 package seedu.address.ui;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,16 +16,12 @@ import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.task.Task;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.logging.Logger;
-
+/**
+ * Calendar. Still under construction.
+ */
 public class CalendarBox extends UiPart<Region> {
     private static final String FXML = "Calendar.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
-    private final ObservableList<Task> taskList;
 
     @FXML
     private Label month;
@@ -38,7 +40,6 @@ public class CalendarBox extends UiPart<Region> {
 
     public CalendarBox(ObservableList<Task> taskList) {
         super(FXML);
-        this.taskList = taskList;
         Date date = new Date();
         LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         month.setText(localDate.getMonth().toString());
@@ -49,8 +50,8 @@ public class CalendarBox extends UiPart<Region> {
         //day of week of first day
         int dayOfWeek = localDate.withDayOfMonth(1).getDayOfWeek().getValue();
 
-        for(int i = 1, j= 1; i<= localDate.lengthOfMonth(); i++) {
-            if(dayOfWeek == 7) {
+        for (int i = 1, j = 1; i <= localDate.lengthOfMonth(); i++) {
+            if (dayOfWeek == 7) {
                 dayOfWeek = 0;
                 j++;
             }
@@ -58,11 +59,12 @@ public class CalendarBox extends UiPart<Region> {
             label = new Label(" " + i);
             temp.getChildren().add(label);
             LocalDate tempDate = LocalDate.of(localDate.getYear(), localDate.getMonth(), i);
-            for(Task task : taskList) {
+            for (Task task : taskList) {
                 LocalDateTime[] ldt = task.getDateTimes();
                 LocalDateTime tempTaskDueDate = ldt[0];
-                LocalDate taskDueDate = LocalDate.of(tempTaskDueDate.getYear(), tempTaskDueDate.getMonth(), tempTaskDueDate.getDayOfMonth());
-                if(taskDueDate.equals(tempDate)) {
+                LocalDate taskDueDate = LocalDate.of(tempTaskDueDate.getYear(),
+                        tempTaskDueDate.getMonth(), tempTaskDueDate.getDayOfMonth());
+                if (taskDueDate.equals(tempDate)) {
                     temp.getChildren().add(new Label(task.getTaskName()));
                 }
             }
