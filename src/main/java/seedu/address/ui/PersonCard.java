bf93aa48cc9f8f0f7f11a8fa44c6140a86da1bae
Wang Yuting <e0365980@u.nus.edu>
2020-03-22 09:44:23 +0800

    UI update and code refactor

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -13,7 +13,7 @@ import seedu.address.model.task.Task;
  */
 public class PersonCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "TaskListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
