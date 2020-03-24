8cfe2d823d78d5cb4b0a3080718af97858268a05
gx-huang <ahuanggx@gmail.com>
2020-03-24 21:47:44 +0800

    Updated developers guide

--- src/main/java/seedu/address/ui/CalendarBox.java
+++ src/main/java/seedu/address/ui/CalendarBox.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.logging.Logger;
@@ -29,6 +30,9 @@ import seedu.address.model.task.Task;
 public class CalendarBox extends UiPart<Region> {
     private static final String FXML = "Calendar.fxml";
     private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+    private Logic logic;
+    private int calendarYear;
+    private Month calendarMonth;
 
     @FXML
     private Label month;
@@ -47,17 +51,31 @@ public class CalendarBox extends UiPart<Region> {
 
     public CalendarBox(Logic logic) {
         super(FXML);
+        this.logic = logic;
         Date date = new Date();
         LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        month.setText(localDate.getMonth().toString());
-        year.setText(String.valueOf(localDate.getYear()));
+        generateCalendar(localDate.getYear(), localDate.getMonth());
+    }
+
+    /**
+     * Generates a calendarBox with give year and month
+     * @param calYear calendar year
+     * @param calMonth calendar month
+     */
+    private void generateCalendar(int calYear, Month calMonth) {
+
+        month.setText(calMonth.toString());
+        year.setText(String.valueOf(calYear));
+        calendarMonth = calMonth;
+        calendarYear = calYear;
 
         Label label;
+        LocalDate newDate = LocalDate.of(calYear, calMonth, 1);
 
         //day of week of first day
-        int dayOfWeek = localDate.withDayOfMonth(1).getDayOfWeek().getValue();
+        int dayOfWeek = newDate.getDayOfWeek().getValue();
 
-        for (int i = 1, j = 0; i <= localDate.lengthOfMonth(); i++) {
+        for (int i = 1, j = 0; i <= newDate.lengthOfMonth(); i++) {
             if (dayOfWeek == 7) {
                 dayOfWeek = 0;
                 j++;
@@ -68,7 +86,7 @@ public class CalendarBox extends UiPart<Region> {
             monthBox.add(p, dayOfWeek, j);
             label = new Label(" " + i);
             temp.getChildren().add(label);
-            LocalDate tempDate = LocalDate.of(localDate.getYear(), localDate.getMonth(), i);
+            LocalDate tempDate = LocalDate.of(newDate.getYear(), newDate.getMonth(), i);
             for (Task task : logic.getFilteredTaskList()) {
                 LocalDateTime[] ldt = task.getDateTimes();
                 LocalDateTime tempTaskDueDate = ldt[0];
@@ -81,5 +99,21 @@ public class CalendarBox extends UiPart<Region> {
             monthBox.add(temp, dayOfWeek, j);
             dayOfWeek++;
         }
+    }
+
+    /**
+     * Generates calendar from previous month.
+     */
+    public void onClickPrevious() {
+        monthBox.getChildren().clear();
+        generateCalendar(this.calendarYear, this.calendarMonth.minus(1));
+    }
+
+    /**
+     * Generates calendar for next month.
+     */
+    public void onClickNext() {
+        monthBox.getChildren().clear();
+        generateCalendar(this.calendarYear, this.calendarMonth.plus(1));
     }
 }
