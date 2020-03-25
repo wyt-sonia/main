a402db865b2a3e72c65b50c02fcbaa94a252085a
gx-huang <ahuanggx@gmail.com>
2020-03-25 23:04:43 +0800

    Major feature: Calendar

--- src/main/java/seedu/address/ui/CalendarBox.java
+++ src/main/java/seedu/address/ui/CalendarBox.java
@@ -7,21 +7,18 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.logging.Logger;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
 import seedu.address.model.task.Task;
+import seedu.address.model.task.UniqueTaskList;
 
 /**
  * Calendar. Still under construction.
@@ -29,10 +26,11 @@ import seedu.address.model.task.Task;
  */
 public class CalendarBox extends UiPart<Region> {
     private static final String FXML = "Calendar.fxml";
-    private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
+    private final Logger logger = LogsCenter.getLogger(CalendarBox.class);
     private Logic logic;
     private int calendarYear;
     private Month calendarMonth;
+    private LocalDate localDate;
 
     @FXML
     private Label month;
@@ -53,7 +51,7 @@ public class CalendarBox extends UiPart<Region> {
         super(FXML);
         this.logic = logic;
         Date date = new Date();
-        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         generateCalendar(localDate.getYear(), localDate.getMonth());
     }
 
@@ -80,13 +78,17 @@ public class CalendarBox extends UiPart<Region> {
                 dayOfWeek = 0;
                 j++;
             }
+            LocalDate tempDate = LocalDate.of(newDate.getYear(), newDate.getMonth(), i);
             VBox temp = new VBox();
             Pane p = new Pane();
             p.setBackground(new Background(new BackgroundFill(Color.WHEAT, CornerRadii.EMPTY, Insets.EMPTY)));
+            if (tempDate.equals(localDate)) {
+                p.setBorder(new Border(new BorderStroke(Color.CYAN,
+                        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+            }
             monthBox.add(p, dayOfWeek, j);
             label = new Label(" " + i);
             temp.getChildren().add(label);
-            LocalDate tempDate = LocalDate.of(newDate.getYear(), newDate.getMonth(), i);
             for (Task task : logic.getFilteredTaskList()) {
                 LocalDateTime[] ldt = task.getDateTimes();
                 LocalDateTime tempTaskDueDate = ldt[0];
@@ -106,7 +108,11 @@ public class CalendarBox extends UiPart<Region> {
      */
     public void onClickPrevious() {
         monthBox.getChildren().clear();
-        generateCalendar(this.calendarYear, this.calendarMonth.minus(1));
+        if (calendarMonth.getValue() == 1) {
+            generateCalendar(this.calendarYear - 1, Month.DECEMBER);
+        } else {
+            generateCalendar(this.calendarYear, this.calendarMonth.minus(1));
+        }
     }
 
     /**
@@ -114,6 +120,63 @@ public class CalendarBox extends UiPart<Region> {
      */
     public void onClickNext() {
         monthBox.getChildren().clear();
-        generateCalendar(this.calendarYear, this.calendarMonth.plus(1));
+        if (calendarMonth.getValue() == 12) {
+            generateCalendar(this.calendarYear + 1, Month.JANUARY);
+        } else {
+            generateCalendar(this.calendarYear, this.calendarMonth.plus(1));
+        }
     }
+
+    /**
+     * Home button. Generates calendar for current month.
+     */
+    public void onClickHome() {
+        monthBox.getChildren().clear();
+        generateCalendar(localDate.getYear(), localDate.getMonth());
+    }
+
+    /**
+     * Generates task list when clicked on cell.
+     * @param event mouse click event
+     */
+    public void onClickDate(javafx.scene.input.MouseEvent event) {
+        Node clickedNode = event.getPickResult().getIntersectedNode();
+        if (clickedNode != monthBox) {
+            Node parent = clickedNode.getParent();
+            while (parent != monthBox) {
+                clickedNode = parent;
+                parent = clickedNode.getParent();
+            }
+            Integer colIndex = GridPane.getColumnIndex(clickedNode);
+            Integer rowIndex = GridPane.getRowIndex(clickedNode);
+            int firstDayOfWeek = LocalDate.of(calendarYear, calendarMonth, 1)
+                    .getDayOfWeek().getValue();
+            if (firstDayOfWeek == 7) {
+                firstDayOfWeek = 0;
+                rowIndex--;
+            }
+            int date = rowIndex * 7 + colIndex - firstDayOfWeek + 1;
+            LocalDate clickedDate = LocalDate.of(calendarYear, calendarMonth, date);
+            ObservableList<Task> taskByDay = generateTaskList(clickedDate);
+            System.out.println(clickedDate);
+            System.out.println(taskByDay.get(0).getTaskName());
+        }
+
+    }
+
+
+    public ObservableList<Task> generateTaskList(LocalDate date) {
+        UniqueTaskList taskByDay = new UniqueTaskList();
+        for (Task task : logic.getFilteredTaskList()) {
+            LocalDateTime[] ldt = task.getDateTimes();
+            LocalDateTime tempTaskDueDate = ldt[0];
+            LocalDate taskDueDate = LocalDate.of(tempTaskDueDate.getYear(),
+                    tempTaskDueDate.getMonth(), tempTaskDueDate.getDayOfMonth());
+            if (taskDueDate.equals(date)) {
+                taskByDay.add(task);
+            }
+        }
+        return taskByDay.asUnmodifiableObservableList();
+    }
+
 }
