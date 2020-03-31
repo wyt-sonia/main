717c3646dc07c444c9f2744aded29a3b2ec48e50
souwmyaa <souwmyaa9@gmail.com>
2020-03-31 10:17:01 +0800

    UG

--- src/test/java/draganddrop/studybuddy/testutil/TypicalIndexes.java
+++ src/test/java/draganddrop/studybuddy/testutil/TypicalIndexes.java
@@ -8,5 +8,4 @@ import draganddrop.studybuddy.commons.core.index.Index;
 public class TypicalIndexes {
     public static final Index INDEX_FIRST_TASK = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_TASK = Index.fromOneBased(2);
-    public static final Index INDEX_THIRD_TASK = Index.fromOneBased(3);
 }
