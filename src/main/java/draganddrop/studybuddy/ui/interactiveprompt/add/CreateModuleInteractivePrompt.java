9d78a39c9a9387d932bd00a10c6057ede0a733b0
Wang Yuting <e0365980@u.nus.edu>
2020-04-02 17:49:14 +0800

    code style

--- src/main/java/draganddrop/studybuddy/ui/interactiveprompt/add/CreateModuleInteractivePrompt.java
+++ src/main/java/draganddrop/studybuddy/ui/interactiveprompt/add/CreateModuleInteractivePrompt.java
@@ -16,7 +16,7 @@ import draganddrop.studybuddy.ui.interactiveprompt.InteractivePromptType;
  * An interactive prompt for creating modules.
  */
 public class CreateModuleInteractivePrompt extends InteractivePrompt {
-    final public static String QUIT_COMMAND_MSG = "Successfully quited from create mod command.";
+    public final static String QUIT_COMMAND_MSG = "Successfully quited from create mod command.";
     private Module module;
 
     public CreateModuleInteractivePrompt() {
