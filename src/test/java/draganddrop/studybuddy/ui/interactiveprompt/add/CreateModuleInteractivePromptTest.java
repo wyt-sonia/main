e5badc41e654b953097bb5cff65a072068302eae
Wang Yuting <e0365980@u.nus.edu>
2020-04-10 15:39:00 +0800

    code style

--- src/test/java/draganddrop/studybuddy/ui/interactiveprompt/add/CreateModuleInteractivePromptTest.java
+++ src/test/java/draganddrop/studybuddy/ui/interactiveprompt/add/CreateModuleInteractivePromptTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
-import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
