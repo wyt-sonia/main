d1ba7bc98d3213fc8a0db08f82d56fc3a23bf392
teikjun <teikjunhci@gmail.com>
2020-04-11 22:27:51 +0800

    add javadocs

--- src/main/java/draganddrop/studybuddy/model/statistics/GoalObserver.java
+++ src/main/java/draganddrop/studybuddy/model/statistics/GoalObserver.java
@@ -1,5 +1,11 @@
 package draganddrop.studybuddy.model.statistics;
 
+/**
+ * Observes the goal field in generalStats
+ */
 public interface GoalObserver {
+    /**
+     * updates the observer when goal is changed
+     */
     public void update();
 }
