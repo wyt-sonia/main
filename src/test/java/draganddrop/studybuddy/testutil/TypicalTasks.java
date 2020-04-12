983b44feef13c708d9af3c95eebb92860f1ece9e
Wang Yuting <e0365980@u.nus.edu>
2020-04-12 11:06:37 +0800

    codestyle

--- src/test/java/draganddrop/studybuddy/testutil/TypicalTasks.java
+++ src/test/java/draganddrop/studybuddy/testutil/TypicalTasks.java
@@ -16,13 +16,13 @@ import draganddrop.studybuddy.model.task.TaskType;
 public class TypicalTasks {
 
     private static final String LONG_DESC_WITH_300_CHAR =
-            "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567"
-             + "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567"
-             + "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567";
+        "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567"
+            + "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567"
+            + "This is a long task description with 300 characters. 12345678901234567890123456789012345678901234567";
     private static final String SHORT_DESC = "This is a short description.";
 
     private static final Module emptyModule = new EmptyModule();
-    private static final  Module cs2103T = new Module("Software Engineering", "CS2103T");
+    private static final Module cs2103T = new Module("Software Engineering", "CS2103T");
     private static final Module cs2101 = new Module("Effective Communication for Computing Professionals",
         "CS2101");
 
@@ -41,21 +41,20 @@ public class TypicalTasks {
         LocalDateTime.now().plusDays(17)};
 
 
-
     public static Task[] getSampleTasks() {
 
         return new Task[]{
             new Task(emptyModule, TaskType.Others, "Homework 1", SHORT_DESC, 20.0,
-                    TaskStatus.PENDING, pendingDuration1, 5.0, pastDateTime1),
+                TaskStatus.PENDING, pendingDuration1, 5.0, pastDateTime1),
             new Task(emptyModule, TaskType.Presentation, "Leadership Presentation 1",
-                    LONG_DESC_WITH_300_CHAR, 0.0, TaskStatus.FINISHED, dueSoonDuration1,
-                    3.0, pastDateTime1),
+                LONG_DESC_WITH_300_CHAR, 0.0, TaskStatus.FINISHED, dueSoonDuration1,
+                3.0, pastDateTime1),
             new Task(cs2101, TaskType.Assignment, "CS2101 Quiz 1", SHORT_DESC,
-                    15.0, TaskStatus.PENDING, dateTimesOne, 2.0, pastDateTime1),
+                15.0, TaskStatus.PENDING, dateTimesOne, 2.0, pastDateTime1),
             new Task(cs2101, TaskType.Meeting, "Group Meeting 1", LONG_DESC_WITH_300_CHAR, 15.0,
-                    TaskStatus.FINISHED, dueSoonDuration1, 5.0, pastDateTime1),
+                TaskStatus.FINISHED, dueSoonDuration1, 5.0, pastDateTime1),
             new Task(cs2101, TaskType.Quiz, "CS2101 Quiz 1", SHORT_DESC,
-                    15.0, TaskStatus.PENDING, pendingDuration1, 2.0, pastDateTime1)
+                15.0, TaskStatus.PENDING, pendingDuration1, 2.0, pastDateTime1)
         };
     }
 
@@ -63,16 +62,16 @@ public class TypicalTasks {
 
         return new Task[]{
             new Task(cs2103T, TaskType.Assignment, "Ass 2", "taskDescription", 20.0,
-                    TaskStatus.PENDING, dateTimesOne, 5.0, creationDateTime),
+                TaskStatus.PENDING, dateTimesOne, 5.0, creationDateTime),
             new Task(cs2101, TaskType.Presentation, "Presentation 2",
-                    "Presentation taskDescription", 20.0, TaskStatus.FINISHED, dateTimesOne,
-                    3.0, creationDateTime),
+                "Presentation taskDescription", 20.0, TaskStatus.FINISHED, dateTimesOne,
+                3.0, creationDateTime),
             new Task(cs2103T, TaskType.Assignment, "Quiz 2", "Quiz taskDescription",
-                    2.0, TaskStatus.PENDING, dateTimesTwo, 1.0, creationDateTime),
+                2.0, TaskStatus.PENDING, dateTimesTwo, 1.0, creationDateTime),
             new Task(cs2101, TaskType.Meeting, "Meeting 2", "Meeting desc", 20.0,
-                    TaskStatus.PENDING, dateTimesTwo, 15.0, creationDateTime),
+                TaskStatus.PENDING, dateTimesTwo, 15.0, creationDateTime),
             new Task(cs2103T, TaskType.Assignment, "Quiz 2", "Quiz taskDescription",
-                    2.0, TaskStatus.PENDING, dateTimesTwo, 1.0, creationDateTime),
+                2.0, TaskStatus.PENDING, dateTimesTwo, 1.0, creationDateTime),
         };
     }
 
